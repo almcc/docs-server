e2510d49b90e8ff4e1ae43b6c167021f9c82b1dd
Alastair McClelland <alastair.mcclelland@gmail.com>
2015-12-02 19:32:31 +0000

    Now using postgresql

--- oddjob/oddjob/settings.py
+++ oddjob/oddjob/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'oddjob.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
--- oddjob/start-app.sh
+++ oddjob/start-app.sh
@@ -1,3 +1,4 @@
+sleep 1
 python manage.py migrate
 python manage.py loaddata fixtures/*
 python manage.py runserver 0.0.0.0:8000
